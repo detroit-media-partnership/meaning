376141f1544a60ddfee6d42515f928df788f28fb
Eric Bower <neurosnap@gmail.com>
2014-05-06 15:00:50 -0400

    fleshed out html templates

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from forms import MeaningForm
 
 app = Flask(__name__)
 app.debug = True
+app.secret_key ='\xa2\xf2\xa8\xdb/\xf3\xae\xdd\xf6\xe2u"Ph\x12:\xa8\xa2\xf8:\x8fTMr'
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -17,16 +18,20 @@ def shutdown_session(exception=None):
 def intro():
 	return render_template('intro.html')
 
-@app.route('/submit')
+@app.route('/survey')
 def submit():
 	form = MeaningForm()
-	return render_template('submit.html', form=form)
+	return render_template('survey.html', form=form)
 
-@app.route('complete', methods=['GET', 'POST'])
+@app.route('/complete', methods=['GET', 'POST'])
 def complete():
 	form = MeaningForm()
 	if not form.validate_on_submit():
 		pass
+
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+	return render_template('results.html')
 
 if __name__ == '__main__':
 	app.run()

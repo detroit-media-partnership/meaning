00e311804a77967aeaf85659947100d449a5ab02
Eric Bower <neurosnap@gmail.com>
2014-05-09 12:05:04 -0700

    updated slider

--- static/js/survey.js
+++ static/js/survey.js
@@ -1,9 +1,3 @@
 $(function() {
-	$('.sliders').slider({
-		"min": 0,
-		"max": 100,
-		"value": 50
-	}).on("slide", function(ev) {
-		$("#" + ev.target.id).parent().parent().parent().find(".slider-value").html("Value: " + $("#" + ev.target.id).data("slider").getValue());
-	});
+	$('.sliders').slider();
 });

13620a3813dd1ccce1dd3342c9bf81d4df2512f1
Eric Bower <neurosnap@gmail.com>
2014-05-09 15:19:58 -0700

    added sqlalchemy ping method to help with mysql has gone away

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, exc, event
+from sqlalchemy.pool import Pool
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import db_uri
@@ -14,3 +15,12 @@ Base.query = db_session.query_property()
 def init_db():
 	import models
 	Base.metadata.create_all(bind=engine)
+
+@event.listens_for(Pool, "checkout")
+def ping_connection(dbapi_connection, connection_record, connection_proxy):
+	cursor = dbapi_connection.cursor()
+	try:
+		cursor.execute("SELECT 1")
+	except:
+		raise exc.DisconnectionError()
+	cursor.close()
